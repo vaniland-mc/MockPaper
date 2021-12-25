0d826b0c5d3cd071d7f5e4df39811b671f311ffc
rona-tombo <ronatombo@gmail.com>
2021-12-25 11:43:45 +0900

    ➕ Add paper-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.6.10"
 }
@@ -7,8 +9,39 @@ version = "0.1.0"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "PaperMC"
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+        content {
+            includeGroup("io.papermc.paper")
+        }
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("io.papermc.paper:paper-api:1.17.1-R0.1-20211219.175449-302")
+}
+
+val targetJavaVersion = 17
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            this.jvmTarget = "$targetJavaVersion"
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
