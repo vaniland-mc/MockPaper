e403c42c605907b50f024e09fb0960560656c3c4
rona-tombo <ronatombo@gmail.com>
2022-01-02 18:36:38 +0900

    📝 Add KDoc for MessageTarget

--- src/main/kotlin/land/vani/mockpaper/command/MessageTarget.kt
+++ src/main/kotlin/land/vani/mockpaper/command/MessageTarget.kt
@@ -4,13 +4,22 @@ import kotlin.test.assertEquals
 import kotlin.test.fail
 
 interface MessageTarget {
+    /**
+     * Returns the next message that was sent to the target.
+     */
     fun nextMessage(): String?
 
+    /**
+     * Asserts that a specific message was not received next by the message target.
+     */
     infix fun assertSaid(expected: String) {
         val message = nextMessage() ?: fail("No more messages were sent.")
         assertEquals(expected, message)
     }
 
+    /**
+     * Asserts that no more messages were received by the message target.
+     */
     fun assertNoMoreSaid() {
         if (nextMessage() != null) {
             fail("More messages were available.")
