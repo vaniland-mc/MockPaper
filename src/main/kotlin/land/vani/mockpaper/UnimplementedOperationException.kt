89af0f9ec9573017d5ef633f5981383b6b63895e
rona-tombo <ronatombo@gmail.com>
2022-01-02 21:59:54 +0900

    📝 Add KDoc for UnimplementedOperationException

--- src/main/kotlin/land/vani/mockpaper/UnimplementedOperationException.kt
+++ src/main/kotlin/land/vani/mockpaper/UnimplementedOperationException.kt
@@ -2,4 +2,13 @@ package land.vani.mockpaper
 
 import org.opentest4j.TestAbortedException
 
-class UnimplementedOperationException(override val message: String? = null) : TestAbortedException()
+/**
+ * Sometimes your code may use a method that is not yet implemented in MockPaper.
+ *
+ * Then this happens MockPaper will, instead o returning placeholder values, throw an [UnimplementedOperationException].
+ *
+ * This is a subclass of [TestAbortedException] and causes your test to be skipped instead of just failing.
+ */
+class UnimplementedOperationException
+@JvmOverloads
+constructor(override val message: String? = null) : TestAbortedException()
