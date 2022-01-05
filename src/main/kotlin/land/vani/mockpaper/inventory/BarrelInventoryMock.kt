4b126c8218a06848b3a98d2d5a9e1907ca13798d
rona-tombo <ronatombo@gmail.com>
2022-01-05 15:43:45 +0900

    🧑‍💻 Make BarrelInventoryMock holder nullable

--- src/main/kotlin/land/vani/mockpaper/inventory/BarrelInventoryMock.kt
+++ src/main/kotlin/land/vani/mockpaper/inventory/BarrelInventoryMock.kt
@@ -5,8 +5,8 @@ import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 
-class BarrelInventoryMock(holder: InventoryHolder) : InventoryMock(holder, 27, InventoryType.BARREL) {
-    override fun getSnapshot(): Inventory = BarrelInventoryMock(holder!!).apply {
+class BarrelInventoryMock(holder: InventoryHolder?) : InventoryMock(holder, 27, InventoryType.BARREL) {
+    override fun getSnapshot(): Inventory = BarrelInventoryMock(holder).apply {
         setContents(contents.fallbackNull())
     }
 }
