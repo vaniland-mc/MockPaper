1d064c15cac786de71f0c31969cabd4c780e3569
rona-tombo <ronatombo@gmail.com>
2022-01-11 00:56:58 +0900

    ✅ Add ExperienceOrbTest

---
+++ src/test/kotlin/land/vani/mockpaper/entity/ExperienceOrbTest.kt
@@ -0,0 +1,43 @@
+package land.vani.mockpaper.entity
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldBe
+import land.vani.mockpaper.ServerMock
+import land.vani.mockpaper.randomLocation
+import land.vani.mockpaper.world.WorldMock
+import org.bukkit.entity.EntityType
+import java.util.UUID
+
+class ExperienceOrbTest : ShouldSpec({
+    lateinit var server: ServerMock
+    lateinit var world: WorldMock
+    lateinit var orb: ExperienceOrbMock
+
+    beforeTest {
+        server = ServerMock()
+        world = server.addSimpleWorld("world")
+        orb = world.spawn(randomLocation(world))
+    }
+
+    should("entityType") {
+        orb.type shouldBe EntityType.EXPERIENCE_ORB
+    }
+
+    should("spawning") {
+        world.entities shouldContain orb
+        orb.experience shouldBeExactly 0
+    }
+
+    should("second constructor") {
+        orb = ExperienceOrbMock(server, UUID.randomUUID(), 10)
+        orb.experience shouldBeExactly 10
+    }
+
+    should("set experience") {
+        orb.experience shouldBeExactly 0
+        orb.experience = 10
+        orb.experience shouldBeExactly 10
+    }
+})
