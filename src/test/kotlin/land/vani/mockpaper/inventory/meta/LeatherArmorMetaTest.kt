58438b1bd1ebcb4830bcf9e7a1f20281717a078e
rona-tombo <ronatombo@gmail.com>
2022-01-19 04:18:12 +0900

    ✅ Add LeatherArmorMetaTest

---
+++ src/test/kotlin/land/vani/mockpaper/inventory/meta/LeatherArmorMetaTest.kt
@@ -0,0 +1,40 @@
+package land.vani.mockpaper.inventory.meta
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import land.vani.mockpaper.ServerMock
+import org.bukkit.Color
+
+class LeatherArmorMetaTest : ShouldSpec({
+    lateinit var server: ServerMock
+    lateinit var meta: LeatherArmorMetaMock
+
+    beforeTest {
+        server = ServerMock()
+        meta = LeatherArmorMetaMock()
+    }
+
+    should("getColor is default") {
+        meta.color shouldBe server.itemFactory.defaultLeatherColor
+    }
+
+    should("setColor") {
+        meta.setColor(Color.MAROON)
+
+        meta.color shouldBe Color.MAROON
+    }
+
+    should("reset color") {
+        meta.setColor(Color.MAROON)
+        meta.setColor(null)
+
+        meta.color shouldBe server.itemFactory.defaultLeatherColor
+    }
+
+    should("clone") {
+        val meta2 = meta.clone()
+
+        meta shouldBe meta2
+        meta2 shouldBe meta
+    }
+})
