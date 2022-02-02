e5fa8d8eee3948297f0d46ce0dd7b6ae94740089
rona-tombo <ronatombo@gmail.com>
2022-02-02 15:40:20 +0900

    ✅ Add RepeatedTaskTest

--- src/main/kotlin/land/vani/mockpaper/scheduler/RepeatedTaskMock.kt
+++ src/main/kotlin/land/vani/mockpaper/scheduler/RepeatedTaskMock.kt
@@ -1,15 +1,18 @@
 package land.vani.mockpaper.scheduler
 
 import org.bukkit.plugin.Plugin
+import org.jetbrains.annotations.VisibleForTesting
 
 class RepeatedTaskMock(
     id: Int,
     plugin: Plugin,
     isSync: Boolean,
     scheduledTick: Long,
+    @VisibleForTesting
     val period: Long,
     runnable: Runnable,
 ) : ScheduledTaskMock(id, plugin, isSync, scheduledTick, runnable) {
+    @VisibleForTesting
     fun updateScheduledTick() {
         scheduledTick += period
     }
