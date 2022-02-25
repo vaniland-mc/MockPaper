965a9bdef9d5ed6f960f54dcdd531ad3df586fcd
rona-tombo <ronatombo@gmail.com>
2022-02-25 11:07:18 +0900

    🔧 Apply dokka to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.dokka")
 
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
